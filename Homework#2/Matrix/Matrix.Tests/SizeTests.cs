890764dd0afa6bcb83e241f3dc9a8693b15d5179
iryna.kushnir <irochka.kushnir@gmail.com>
2015-10-18 09:49:13 -0700

    Adding Size class implementation and new tests

---
+++ Homework#2/Matrix/Matrix.Tests/SizeTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace Matrix.Tests
+{
+    public class SizeTests
+    {
+        [Test]
+        public void WidthProperty_Always_ReturnsWidth()
+        {
+            var size = new Size(1, 2);
+
+            Assert.AreEqual(1, size.Width);
+        }
+
+        [Test]
+        public void HeightProperty_Always_ReturnsHeight()
+        {
+            var size = new Size(1, 2);
+
+            Assert.AreEqual(2, size.Height);
+        }
+
+        [Test]
+        public void IsSquare_WhenHeightIsTheSameAsWidth_ReturnsTrue()
+        {
+            var size = new Size(1, 1);
+
+            Assert.IsTrue(size.IsSquare);
+        }
+
+        [Test]
+        public void IsSquare_WhenHeightAndWidthAreDifferent_ReturnsFalse()
+        {
+            var size = new Size(1, 2);
+
+            Assert.IsFalse(size.IsSquare);
+        }
+
+        [Test]
+        public void EqualsOperator_WhenHeightIsTheSameAsWidth_ReturnsTrue()
+        {
+            var sizeA = new Size(1, 1);
+            var sizeB = new Size(1, 1);
+
+            Assert.IsTrue(sizeA == sizeB);
+        }
+
+        [Test]
+        public void EqualsOperator_WhenHeightAndWidthAreDifferent_ReturnsFalse()
+        {
+            var sizeA = new Size(1, 1);
+            var sizeB = new Size(1, 2);
+            var sizeC = new Size(2, 1);
+
+            Assert.IsFalse(sizeA == sizeB);
+            Assert.IsFalse(sizeA == sizeC);
+            Assert.IsFalse(sizeB == sizeC);
+        }
+
+        [Test]
+        public void EqualsOperator_ForTheSameObject_ReturnsTrue()
+        {
+            var size = new Size(1, 2);
+
+            Assert.IsTrue(size == size);
+        }
+
+        [Test]
+        public void EqualsOperator_ForNull_ReturnsFalse()
+        {
+            var size = new Size(1, 2);
+
+            Assert.IsFalse(size == null);
+        }
+
+        [Test]
+        public void GetHashCode_ForEqualObjects_IsTheSame()
+        {
+            var size1 = new Size(1, 1);
+            var size2 = new Size(1, 1);
+
+            Assert.AreEqual(size1, size2);
+            Assert.AreEqual(size1.GetHashCode(), size2.GetHashCode());
+        }
+    }
+}
---
+++ Homework#2/Matrix/Matrix/Size.cs
@@ -0,0 +1,43 @@
+﻿namespace Matrix
+{
+    public class Size
+    {
+        public Size(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool IsSquare => Width == Height;
+
+        protected bool Equals(Size other)
+        {
+            return Width == other.Width && Height == other.Height;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Size) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Width ^ Height;
+        }
+
+        public static bool operator ==(Size size1, Size size2)
+        {
+            return Equals(size1, size2);
+        }
+
+        public static bool operator !=(Size size1, Size size2)
+        {
+            return !Equals(size1, size2);
+        }
+    }
+}
