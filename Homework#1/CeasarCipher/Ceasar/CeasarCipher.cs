0b49bc01df67c1ec8cfd9cc852a1dd56b42f0995
unknown <irk@pc-irk.kyiv.ciklum.net>
2015-11-18 14:02:23 +0200

    Fixing Ceasar Cipher according to comments

--- Homework#1/CeasarCipher/Ceasar/CeasarCipher.cs
+++ Homework#1/CeasarCipher/Ceasar/CeasarCipher.cs
@@ -16,56 +16,14 @@ namespace Ceasar.Cipher
 
         public string Encrypt(string plaintext)
         {
-            if (plaintext == null) throw new ArgumentNullException(nameof(plaintext), "Plaintext can't be NULL");
-            var plaintextBytes = Encoding.ASCII.GetBytes(plaintext);
-            foreach (var b in plaintextBytes)
-            {
-                if (b < startASCIIIndex || b > endASCIIIndex)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(plaintext), "Plaintext contains forbidden symbols");
-                }
-            }
-            var ciphertextBytes = new byte[plaintext.Length];
-            for (var i = 0; i < ciphertextBytes.Length; i++)
-            {
-                var encryptedTemp = Mod(plaintextBytes[i] + _offset, endASCIIIndex);
-                if (encryptedTemp >= startASCIIIndex)
-                {
-                    ciphertextBytes[i] = BitConverter.GetBytes(encryptedTemp)[0];
-                }
-                else
-                {
-                    ciphertextBytes[i] = BitConverter.GetBytes(encryptedTemp + startASCIIIndex - 1)[0];
-                }
-            }
-            return Encoding.ASCII.GetString(ciphertextBytes);
+            ValidateInputString(plaintext);
+            return Shift(plaintext, _offset);
         }
 
         public string Decrypt(string ciphertext)
         {
-            if (ciphertext == null) throw new ArgumentNullException(nameof(ciphertext), "Ciphertext can't be NULL");
-            var ciphertextBytes = Encoding.ASCII.GetBytes(ciphertext);
-            foreach (var b in ciphertextBytes)
-            {
-                if (b < startASCIIIndex || b > endASCIIIndex)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(ciphertext), "Ciphertext contains forbidden symbols");
-                }
-            }
-            var plaintextBytes = new byte[ciphertext.Length];
-            for (var i = 0; i < plaintextBytes.Length; i++)
-            {
-                var decryptedTemp = Mod(ciphertextBytes[i] - _offset, endASCIIIndex);
-                if (decryptedTemp >= startASCIIIndex)
-                {
-                    plaintextBytes[i] = BitConverter.GetBytes(decryptedTemp)[0];
-                }
-                else
-                {
-                    plaintextBytes[i] = BitConverter.GetBytes(endASCIIIndex - startASCIIIndex + decryptedTemp + 1)[0];
-                }
-            }
-            return Encoding.ASCII.GetString(plaintextBytes);
+            ValidateInputString(ciphertext);
+            return Shift(ciphertext, _offset*-1);
         }
 
         public static int Mod(int number, int modBase)
@@ -76,6 +34,45 @@ namespace Ceasar.Cipher
                 mod += modBase;
             }
             return mod;
+        }
+
+        public void ValidateInputString(string stringToValidate)
+        {
+            if (stringToValidate == null)
+                throw new ArgumentNullException(nameof(stringToValidate), "Input string can't be NULL");
+            var stringToValidateBytes = Encoding.ASCII.GetBytes(stringToValidate);
+            foreach (var b in stringToValidateBytes)
+            {
+                if (b < startASCIIIndex || b > endASCIIIndex)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(stringToValidate),
+                        "Input string contains forbidden symbols");
+                }
+            }
+        }
+
+        public string Shift(string stringToShift, int offset)
+        {
+            if (offset == 0) return stringToShift;
+            var inputBytes = Encoding.ASCII.GetBytes(stringToShift);
+            var outputBytes = new byte[stringToShift.Length];
+            for (var i = 0; i < outputBytes.Length; i++)
+            {
+                var temp = Mod(inputBytes[i] + offset, endASCIIIndex);
+                if (temp >= startASCIIIndex)
+                {
+                    outputBytes[i] = BitConverter.GetBytes(temp)[0];
+                }
+                else
+                {
+                    outputBytes[i] =
+                        BitConverter.GetBytes(temp +
+                                              (offset > 0
+                                                  ? (startASCIIIndex - 1)
+                                                  : (endASCIIIndex - startASCIIIndex + 1)))[0];
+                }
+            }
+            return Encoding.ASCII.GetString(outputBytes);
         }
     }
 }
